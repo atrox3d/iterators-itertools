d753220607bf3baee73ca4a45d29dc4ef14cf069
atrox3d <atrox3d@gmail.com>
2022-12-13 15:58:21 +0100

    two classes

--- main-iterators.py
+++ main-iterators.py
@@ -14,7 +14,22 @@ class RepeaterIterator:
         return self.source.value
 
 
-repeater = Repeater("Hello")
-
-for item in repeater:
+repeater1 = Repeater("Hello for")
+count = 0
+for item in repeater1:
     print(item)
+    count += 1
+    if count > 5:
+        break
+
+repeater2 = Repeater("Hello while")
+iterator2 = repeater2.__iter__()
+count = 0
+while count < 5:
+    item = iterator2.__next__()
+    print(item)
+    count += 1
+
+iterator1 = repeater1.__iter__()
+print(next(iterator1))
+print(next(iterator2))
